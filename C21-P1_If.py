4b9b19185f71d2dc25ffd7a0f15246cd2f08db04
Arkano007 <144633250+Arkano007@users.noreply.github.com>
2023-09-17 20:11:16 -0600

    Práctica 21

---
+++ C21-P1_If.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 20:05:59 2023
+
+@author: PC
+"""
+
+#Cambia el operador para que la condición sea True.
+    #Number_1 = 15
+    #Number_2 = 20
+    #if Number_1 == Number_2:
+    #	print('Se ejecuta el if 1.')
+    
+Number_1 = 15
+Number_2 = 20
+if Number_1 != Number_2:
+    print('Se ejecuta el if 1.')
+
+
+#Cambia el operador para que la condición sea True.
+#Number_1 = 1450
+#Number_2 = 60
+#if Number_1 < Number_2:
+#	print('Se ejecuta el if 2.')
+
+Number_1 = 1450
+Number_2 = 60
+if Number_1 > Number_2:
+	print('Se ejecuta el if 2.')
+   
+
+# Haz que el siguiente condicional se convierta en False sin cambiar el operador.
+#Number_1 = 1450
+#Number_2 = 60
+#if Number_1 != Number_2:
+#	print('Se ejecuta el if 3.')
+
+Number_1 = 60
+Number_2 = 60
+if Number_1 != Number_2:
+	print('Se ejecuta el if 3.')
