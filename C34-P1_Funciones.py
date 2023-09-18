c03c3da505585c6ae25f9b66c99bb2629aecf783
Arkano007 <144633250+Arkano007@users.noreply.github.com>
2023-09-17 21:52:51 -0600

    Práctica 34

---
+++ C34-P1_Funciones.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 21:47:38 2023
+
+@author: PC
+"""
+
+#Crea una función que realice una suma. Para ello, tendrás que añadir dos argumentos (numero1 y numero2). 
+#En su interior, especificarás un print() que muestre el resultado de la suma. Deberás hacer tres llamadas 
+#que como resultado de la suma den los valores 30, 50 y 57000. Los números que introduzcas en la llamada pueden 
+#ser los que quieras siempre que coincidan los resultados en el print().
+
+def suma(Num_1, Num_2):
+    print("El resultado de la suma es: ",Num_1 + Num_2)
+
+suma(20,10)
+suma(20,30)
+suma(50000,7000)
