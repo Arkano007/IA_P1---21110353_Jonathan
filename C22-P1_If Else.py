b14cb786202e0a424d2517404ec694b4bf0d1ea6
Arkano007 <144633250+Arkano007@users.noreply.github.com>
2023-09-17 20:16:59 -0600

    Práctica 22

---
+++ C22-P1_If Else.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 20:15:01 2023
+
+@author: PC
+"""
+
+#Corrige el siguiente condicional if else.
+
+#color = rojo
+#else color == rojo
+#Print "El color es rojo."
+#if color != rojo
+#Print "El color no es rojo."
+
+Color = "Red" #añadimos comillas para inicializar un string
+
+if Color == "Red": #comparamos con comillas 
+    print("El color es rojo.") #imprimimos si se cumple la condicion
+else:
+    print("El color no es rojo.") #imprimimos si no se cumple la condicion
