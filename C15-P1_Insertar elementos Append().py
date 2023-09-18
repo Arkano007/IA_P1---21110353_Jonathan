44106532ea95bd2243ffec4346add1a2653fbe9d
Arkano007 <144633250+Arkano007@users.noreply.github.com>
2023-09-17 19:23:45 -0600

    Práctica 15

---
+++ C15-P1_Insertar elementos Append().py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 19:18:29 2023
+
+@author: PC
+"""
+
+#Añade los colores 'fuxia' y 'celeste' a la lista.
+
+Colors = ['Red','Blue','Green','Yellow','Brown','Purple','Black','Pink','White','Orange']
+#declaramos lista
+
+Colors.append('Fuxia') #añadimos color con comillas simples
+Colors.append("Light Blue") #añadimos color con comillas simples
+
+print(Colors) #mostramos con los elementos agregados
