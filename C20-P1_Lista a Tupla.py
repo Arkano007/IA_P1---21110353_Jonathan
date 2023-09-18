ba11e73ee17e717c0035c67a0503439749939f51
Arkano007 <144633250+Arkano007@users.noreply.github.com>
2023-09-17 20:01:14 -0600

    Práctica 20

---
+++ C20-P1_Lista a Tupla.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 19:59:00 2023
+
+@author: PC
+"""
+
+#Convierte la siguiente lista en una tupla y asegúrate que se haya convertido en tupla correctamente
+# imprimiendo en la consola el tipo de elemento que es.
+
+Colors = ['Red','Blue','Green','Yellow','Brown','Purple','Black','Pink','White','Orange']
+#declaramos lista
+
+Tupla = tuple(Colors) #convertimos lista a tupla
+
+print(type(Tupla))#imprimimos el tipo de dato para verificar la conversion
