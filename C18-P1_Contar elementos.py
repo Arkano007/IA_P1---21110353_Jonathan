8520a4ab8500932b1fa8a818ed7f6043a984e267
Arkano007 <144633250+Arkano007@users.noreply.github.com>
2023-09-17 19:44:21 -0600

    Práctica 18

---
+++ C18-P1_Contar elementos.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 19:39:19 2023
+
+@author: PC
+"""
+
+#contar los elementos de una lista
+
+Colors = ['Red','Blue','Green','Yellow'] #declaramos lista
+
+print(len(Colors)) #imprimimos en consola el numero de elementos de la lista sin almacenar el valor
+
+Lon_Color = len(Colors) #almacenamos el numero de elementos en una variable
+
+print(Lon_Color) #mostramos el valor
