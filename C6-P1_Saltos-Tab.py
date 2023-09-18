181c259a9e49a90262fe95e950bc558ded0493a0
Arkano007 <144633250+Arkano007@users.noreply.github.com>
2023-09-17 19:00:18 -0600

    Práctica 6

---
+++ C6-P1_Saltos-Tab.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 17:16:44 2023
+
+@author: PC
+"""
+
+print("-Python.\n-JavaScript.\n-Java.\n-PHP.\n-TypeScript.\n-SQL.\n-COBOL.")
+#imprimimos las palabras con el salto de linea (\n)
+
+print("\t-Python.\n\t-JavaScript.\n\t-Java.\n\t-PHP.\n\t-TypeScript.\n\t-SQL.\n\t-COBOL.")
+#imprimimos, además del salto, con una tabulación(\t)
