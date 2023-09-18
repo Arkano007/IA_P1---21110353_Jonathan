a1f386815f2334777d37449636d96e86611623a8
Arkano007 <144633250+Arkano007@users.noreply.github.com>
2023-09-17 21:01:03 -0600

    Práctica 30

---
+++ C30-P1_For con rango.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 21:00:17 2023
+
+@author: PC
+"""
+
+#Crea un bucle for con un range() que vaya desde el valor 7 hasta el valor 700 en saltos de 100. 
+#Basta con que imprimas el valor de cada iteración.
+
+for x in range(7,700,100): #comienza en 7, hasta el 700, aumentando de 100 en 100
+    print(x) #imprime el valor de x
