07541896f9a77c7d5d584e8de76e9ac5bb39ce85
Arkano007 <144633250+Arkano007@users.noreply.github.com>
2023-09-17 19:08:22 -0600

    Práctica 13

---
+++ C13-P1_Eliminar elementos remove.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 19:05:03 2023
+
+@author: PC
+"""
+
+# Elimina de la siguiente lista los elementos 'amarillo' y 'rojo'.
+# Solo puedes eliminarlos utilizando el método remove().
+
+Colors = ['Red','Blue','Green','Yellow','Brown','Purple','Black','Pink','White','Orange']
+#declaramos lista
+
+Colors.remove('Yellow') #eliminamos el amarillo
+Colors.remove("Red") #eliminamos el rojo
+
+print(Colors)#imprimimos para comprobar las eliminaciones
