ca634bc81a6dabc123599b3167423e82e322e9db
Arkano007 <144633250+Arkano007@users.noreply.github.com>
2023-09-17 19:01:07 -0600

    Práctica 10

---
+++ C10-P1_Listas.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 17:40:01 2023
+
+@author: PC
+"""
+#Crea una lista que contenga los siguientes valores en las posiciones indicadas.
+#"uno" en la posición 4.
+#"dos" en la posición 1.
+#"tres" en la posición 0.
+#"cuatro" en la posición 3.
+#"cinco" en la posición 2.
+
+List = ["tres","dos","cinco","cuatro","uno"] #creamos lista con las posiciones asignadas
+print(List) #imprimimos toda la lista
+print(List[0])#imprimimos el primer dato de la lista
