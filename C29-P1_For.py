6e0d4ad9c71dafbc2fdfa39cd23031ac2816f61c
Arkano007 <144633250+Arkano007@users.noreply.github.com>
2023-09-17 20:59:27 -0600

    Práctica 29

---
+++ C29-P1_For.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 20:56:42 2023
+
+@author: PC
+"""
+
+#Crea un bucle for que itere la siguiente tupla y muestre una frase como esta en cada iteración:
+# 'El color es: ' + color + '.'.
+
+Colors = ('Red','Blue','Green','Yellow')
+
+for x in Colors:
+    print("El color es: " + x + ".")
