227ca31f158b4dc9b6bbf8d215198924bfd1a349
Arkano007 <144633250+Arkano007@users.noreply.github.com>
2023-09-17 19:01:35 -0600

    Práctica 11

---
+++ C11-P1_Posición Negativa.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 18:20:28 2023
+
+@author: PC
+"""
+#Utiliza las posiciones negativas para acceder e imprimir algunos de los colores de esta lista.Acceder a:
+#   'naranja', 'amarillo', 'lila', 'blanco' y 'rojo'.
+
+Colors = ['Red','Blue','Green','Yellow','Brown','Purple','Black','Pink','White','Orange']
+#creamos la lista
+
+print(Colors[-1]) #imprimos el ultimo color/elemento de la lista, color naranja
+print(Colors[3]) #imprimos color amarillo
+print(Colors[-5]) #imprimos color lila
+print(Colors[-2]) #imprimos el penultimo color/elemento de la lista, color blanco
+print(Colors[0]) #imprimimos el color rojo
