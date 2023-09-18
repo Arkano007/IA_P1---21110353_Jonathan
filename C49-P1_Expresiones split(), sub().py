4c0a4f870d22b3c059bd1869a6f62e1b0bea8f01
Arkano007 <144633250+Arkano007@users.noreply.github.com>
2023-09-17 22:38:53 -0600

    Práctica 49

---
+++ C49-P1_Expresiones split(), sub().py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 22:37:07 2023
+
+@author: PC
+"""
+
+import re
+
+texto = "Trina triste traga trigo, trigo traga triste Trina."
+busqueda = re.split(" ", texto)
+print(busqueda)
