84e5c3681d552cd37780bab743a221066da68c29
Arkano007 <144633250+Arkano007@users.noreply.github.com>
2023-09-17 19:39:09 -0600

    Práctica 17

---
+++ C17-P1_Ordenar elementos, Sort(), Sorted().py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 19:34:34 2023
+
+@author: PC
+"""
+
+#Ordena la siguiente lista en orden alfabético descendente (de la letra 'z' a la 'a').
+
+Colors = ['Red','Blue','Green','Yellow','Brown','Purple','Black','Pink','White','Orange']
+#declaramos lista
+
+Colors.sort(reverse=True) #ordenamos con reverse para invertir el olden alfabetico
+
+print(Colors) #mostramos lista ya ordenada 
