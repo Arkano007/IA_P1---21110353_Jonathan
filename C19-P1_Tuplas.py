ce45025bc0c8ff06af0f3fc090fd648b3dcc5a47
Arkano007 <144633250+Arkano007@users.noreply.github.com>
2023-09-17 19:57:16 -0600

    Práctica 19

---
+++ C19-P1_Tuplas.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 19:45:29 2023
+
+@author: PC
+"""
+
+#Imprime la segunda posición de la primer tupla.
+
+Colors = ['Red','Blue','Green','Yellow','Brown','Purple','Black','Pink','White','Orange']
+#declaramos lista
+
+print(Colors[1]) #imprimiendo con el nombre de la tupla y la posicion del elemento
+
+#Utiliza los símbolos de suma y resta para obtener el resultado 25 a partir de los elementos 
+#de la siguiente tupla en una variable llamada operacion.
+
+Number = (10, 1, 5, 11) #nueva dupla
+
+print("la suma es: ", Number[0]+Number[2]+Number[3]-Number[1]) 
+#imprimimos y sumamos/restamos dentro del print los valores de la dupla
